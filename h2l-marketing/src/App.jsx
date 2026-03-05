f37b553a5abd99542ec705df85f114155d240e82
kakzumooto <kakzumooto22@gmail.com>
2026-03-05 11:54:19 -0600

    Cambios en css,index.css

--- h2l-marketing/src/App.jsx
+++ h2l-marketing/src/App.jsx
@@ -1,26 +1,16 @@
-import { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-// Importa tus componentes
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
 function App() {
   useEffect(() => {
     AOS.init({
       duration: 1000,
       once: false,    
     });
+    AOS.refresh();
   }, []);
 
   return (
-    <div className="overflow-x-hidden bg-white font-sans text-[#333333]">
+    <div className="min-h-screen bg-white font-sans text-[#333333]">
       <Navbar />
-      <main>
+      <main className="relative">
         <Hero />
         <Services />
         <Contact />
@@ -29,5 +19,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
